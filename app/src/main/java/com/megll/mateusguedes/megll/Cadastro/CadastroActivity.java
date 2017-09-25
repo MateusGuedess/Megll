4c9dd62bfce1611b5ea2a9f37389447ffab53144
Mateus Guedes <teuguedes@outlook.com>
2017-09-25 18:20:49 -0300

    Finalizando aactivity cadastro e iniciando alterações na Classe activity

--- app/src/main/java/com/megll/mateusguedes/megll/Cadastro/CadastroActivity.java
+++ app/src/main/java/com/megll/mateusguedes/megll/Cadastro/CadastroActivity.java
@@ -2,14 +2,28 @@ package com.megll.mateusguedes.megll.Cadastro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.megll.mateusguedes.megll.R;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private EditText nomeCliente, telefone, celular, documento, email, endereco;
+    private Button btnEnviar, btnCancelar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        nomeCliente = (EditText) findViewById(R.id.nomeCliente);
+        telefone = (EditText) findViewById(R.id.telefone);
+        celular = (EditText) findViewById(R.id.celular);
+        documento = (EditText) findViewById(R.id.email);
+        email = (EditText) findViewById(R.id.endereco);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+
     }
 }
